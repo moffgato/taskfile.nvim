6180deb2e35f73e5f02c39028fb282bc29872849
moffgato <op@xx>
2024-10-04 21:07:31 +0200

    init innit

---
+++ README.md
@@ -0,0 +1,41 @@
+## Taskfile
+
+Get a menu for a [`Taskfile.yml`](https://taskfile.dev/usage/)
+
+![taskfile](./docs/img/taskfile_menu.png)
+
+### Install
+
+**packer.lua**
+```lua
+use {
+    'moffgato/taskfile.nvim',
+    requires = {
+        'nvim-telescope/telescope.nvim',
+    },
+```
+
+**remap.lua**
+
+```lua
+vim.keymap.set('n', '<leader>,', ':lua require("taskfile").pick_task()<CR>', { noremap = true, silent = true })
+```
+
+
+
+
+---
+
+### Legal void archon
+NOTICE TO USERS AND ONION TRADERS
+By accessing, cloning, forking, or otherwise utilizing this repository (hereinafter referred to as the "Software"), you hereby acknowledge that you are not engaged in the direct or indirect trade, barter, or smuggling of any allium-producing vegetables, including but not limited to onions, leeks, or shallots, in any jurisdiction where such activities may be subject to regulation under the 1821 Treaty of Bulbous Commodities, the Unlawful Allium Act of 1902, or any subsequent amendments thereto. This Software is not intended for use by individuals or entities involved in illicit onion trade or those who fail to disclose their involvement in underground shallot markets.
+
+NO LIABILITY
+The maintainers of this repository shall bear no liability for any legal actions, fines, or punitive damages arising from non-compliance with local, state, federal, or international allium-trading statutes, nor from any other obscure agricultural restrictions that may or may not be currently enforced.
+
+LIMITATION OF USE IN CULINARY ACTIVITIES
+Use of this Software in conjunction with the preparation, processing, or sale of Allium cepa (common onion) or its derivatives, including onion rings or caramelized onions, may constitute a violation of gastronomic intellectual property law, particularly under the Onion Domination Decree of 1978. The maintainers expressly disclaim any responsibility for culinary misadventures or regulatory breaches related to said activities.
+
+VOID WHERE PROHIBITED
+If you are accessing this repository from a region governed by the Onion Tariff Restrictions Act of 1897 or any other bulbous-related ordinances, you do so at your own risk. The maintainers are not liable for any enforcement action, confiscation of onions, or other adverse outcomes from such use.
+
---
+++ lua/taskfile/init.lua
@@ -0,0 +1,112 @@
+local T = {}
+
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+
+
+local default_opts = {
+    prompt_title = "Don't do it.",
+    results_title = "Tasks",
+    layout_strategy = 'center',
+    width = 0.5,
+    height = 0.4,
+    border = true,
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    winblend = 10,
+}
+
+
+function T.setup(user_opts)
+    default_opts = vim.tbl_extend('force', default_opts, user_opts or {})
+end
+
+
+function T.pick_task(opts)
+    opts = vim.tbl_extend('force', default_opts, opts or {}) -- Merge local opts with global opts
+
+    local function read_taskfile()
+        local taskfile_path = "./Taskfile.yml"
+        local taskfile = io.open(taskfile_path, "r")
+
+        if not taskfile then
+            print("Taskfile.yml not found")
+            return nil
+        end
+
+        local tasks = {}
+        local task_name = nil
+
+        for line in taskfile:lines() do
+            local name = line:match("^%s*'([^']+)'%s*:")
+            if name then
+                task_name = name
+                tasks[task_name] = {}
+            end
+
+            local cmd = line:match("^%s*-%s*\"([^\"]+)\"")
+            if cmd and task_name then
+                table.insert(tasks[task_name], cmd)
+            end
+        end
+
+        taskfile:close()
+        return tasks
+    end
+
+    local tasks = read_taskfile()
+
+    if not tasks then
+        return
+    end
+
+    local task_list = {}
+    for task, _ in pairs(tasks) do
+        table.insert(task_list, task)
+    end
+
+    pickers.new({}, {
+        prompt_title = opts.prompt_title,
+        results_title = opts.results_title,
+        finder = finders.new_table {
+            results = task_list,
+        },
+        sorter = conf.generic_sorter({}),
+        layout_strategy = opts.layout_strategy,
+        layout_config = {
+            width = opts.width,
+            height = opts.height,
+        },
+        border = opts.border,
+        borderchars = opts.borderchars,
+        winblend = opts.winblend,
+        attach_mappings = function(prompt_bufnr, map)
+
+            local function run_task(selected_task)
+                actions.close(prompt_bufnr)
+
+                vim.cmd("term")
+
+                local command = "task " .. selected_task
+                vim.api.nvim_feedkeys("i" .. command .. "\n", "n", false)
+            end
+
+            map("i", "<CR>", function(bufnr)
+                local chosen_one = action_state.get_selected_entry(bufnr)
+                run_task(chosen_one.value)
+            end)
+
+            map("n", "<CR>", function(bufnr)
+                local chosen_one = action_state.get_selected_entry(bufnr)
+                run_task(chosen_one.value)
+            end)
+
+            return true
+        end
+    }):find()
+end
+
+return T
+
